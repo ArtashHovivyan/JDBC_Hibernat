09c76263d68ceb5acfa91c8e178828682e8becf8
Art <artasheshovivyan@gmail.com>
2025-05-03 20:59:39 +0400

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
+        Home home = new Home();
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
+            home.sum(i,i);
         }
+
+
     }
 }
